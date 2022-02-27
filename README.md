aeb99d367fc9176513aa4434e25ddd6166d2e8cb
Teague Cole <tnc0419@icloud.com>
2022-02-27 17:19:33 -0500

    Finished app, challenges, and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# #100DaysOfSwiftUI Project 5: Word Scramble
+
+[Word Scramble](https://www.hackingwithswift.com/100/swiftui/29) reads a file called starrt.txt which has a list of words. It prompts the user to find all the smaller words inside the root word.  
+ 
+## Screenshot
+![Screenshot](screenshot1.png?raw=true "Word Scramble")
+![Screenshot](screenshot2.png?raw=true "Word Scramble")
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var usedWord = [String]()
-    @State private var rootWord = ""
-    @State private var newWord = ""
+    @State private var rootWord: String = ""
+    @State private var newWord: String = ""
+    @State private var errorTitle: String = ""
+    @State private var errrorMessage: String = ""
+    @State private var showingError: Bool = false
+    @State private var score: Int = 0
     
     var body: some View {
         NavigationView {
@@ -21,7 +25,7 @@ struct ContentView: View {
                         .autocapitalization(.none)
                 }
                 
-                Section {
+                Section(usedWord.count == 1 ? "\(usedWord.count) word found" : "\(usedWord.count) words found") {
                     ForEach(usedWord, id: \.self) { word in
                         HStack {
                             Image(systemName: "\(word.count).circle")
@@ -29,11 +33,58 @@ struct ContentView: View {
                         }
                     }
                 }
+                Text("You have \(score) points")
             }
+            .listStyle(.grouped)
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("Ok", role: .cancel) {}
+            } message: {
+                Text(errrorMessage)
+            }
+            .toolbar {
+                Button("New Game", action: startGame)
+            }
         }
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWord.contains(word)
+    }
+    
+    func isPossible(word :String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func lessThanThreeLetters(word: String) -> Bool {
+        !(word.count < 3)
+    }
+    
+    func isNotRootWord(word: String) -> Bool {
+        word != rootWord
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errrorMessage = message
+        showingError = true
     }
     
     func startGame() {
@@ -41,6 +92,8 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "shouldntHappen"
+                usedWord.removeAll()
+                score = 0
                 return
             }
         }
@@ -51,8 +104,34 @@ struct ContentView: View {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else { return }
         
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from \(rootWord)!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You made up that word!")
+            return
+        }
+        
+        guard lessThanThreeLetters(word: answer) else {
+            wordError(title: "Word is less than 3 letters", message: "Cant use words with less than 3 characters!")
+            return
+        }
+        
+        guard isNotRootWord(word: answer) else {
+            wordError(title: "Word is root word", message: "You cant use \(rootWord)!")
+            return
+        }
+        
         withAnimation {
             usedWord.insert(answer, at: 0)
+            score += answer.count
         }
         newWord = ""
     }
